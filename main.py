8d35da4b168eba15f9137a7eae0fc8ab112a7d8f
dark0on <157805510+dark0on@users.noreply.github.com>
2025-03-13 21:33:04 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+D:\tele_bot\main.py  
+git init
+git remote add origin https://github.com/dark0on/telegram-bot.git
+git add .
+git commit -m "First commit"
+git branch -M main
+git push -u origin main
